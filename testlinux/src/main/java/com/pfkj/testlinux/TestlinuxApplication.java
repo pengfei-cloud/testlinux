77a55f0351ba8c3da659d5ae73245ceb6a5f74c0
PengFei <1115967859@qq.com>
2021-01-06 16:11:05 +0800

    123

---
+++ testlinux/src/main/java/com/pfkj/testlinux/TestlinuxApplication.java
@@ -0,0 +1,13 @@
+package com.pfkj.testlinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestlinuxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestlinuxApplication.class, args);
+    }
+
+}
---
+++ testlinux/src/test/java/com/pfkj/testlinux/TestlinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfkj.testlinux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestlinuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
